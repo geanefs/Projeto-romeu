4002024438fc5688e11cfc3285aa237c901fd5c5
Geane farias <79220570+geanefs@users.noreply.github.com>
2022-09-07 18:38:25 -0300

    Update scketcher.js

--- scketcher.js
+++ scketcher.js
@@ -2,30 +2,32 @@ var dev
 var cli
 
 var cli2
+var cliimg
+
+var ground
 
 var jogo, jogoimg
 
 function preload(){
 
-    //jogoimg = loadImage('assets/game.png.png')
-
+    jogoimg = loadImage('imagens/cenarioOriginal.png')
+    cliimg = loadAnimation('gifs/personagem principal.gif')
 }
 
 
 function setup() {
-    createCanvas(500,500)
+    createCanvas(1000,500)
 
     dev = createSprite(250,400,30,30)
 
-
-    cli = createSprite(250,20,30,30)
+    ground = createSprite(50,490,2000,10)
+    ground.visible = false
+  
 
     
-    cli2 = createSprite(500,20,30,30)
+    //cli.velocity.x = 5
 
-    cli.velocity.x = 5
-
-    cli2.velocity.x = -5
+    //cli2.velocity.x = -5
 
     jogo = createSprite(250,300,20,20)
    // jogo.addImage(jogoimg)
@@ -39,18 +41,11 @@ function setup() {
 }
 
 function draw(){
-    background('green')
+    background(jogoimg)
 
-    if(cli.x > 500){
-        cli.x = 0
-    }
-
-    if(cli2.x < 1){
-        cli2.x = 500
-    }
-    //cli.bounce()
+   
     drawSprites()
-
+   
 
     if(keyIsDown(LEFT_ARROW)){
         dev.position.x -= 10
@@ -58,5 +53,22 @@ function draw(){
     if(keyIsDown(RIGHT_ARROW)){
         dev.position.x += 10
     }
+    if(keyDown(UP_ARROW)){
+        dev.velocityY -= 5
+    }
 
+    dev.velocity.y = dev.velocity.y + 0.8
+
+    dev.collide(ground)
+    Bird()
+   
+}
+
+
+function Bird(){
+if(frameCount % 60 === 0){
+    var pombo = createSprite(1000,150,10,10);
+    pombo.velocityX = -3
+    pombo.y = Math.round(random(10,100))
+}
 }
